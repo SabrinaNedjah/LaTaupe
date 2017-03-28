21a15c70fc90c6cfd617e6cff9cfdd70da736621
Sabrina Nedjah <snedjah78@gmail.com>
2017-03-28 16:03:57 +0200

    avancer du projet

--- script/main.js
+++ script/main.js
@@ -1,6 +1,15 @@
 var compte = 10;
-function decompte()
-{
+var compteurDePoint=0;
+
+if(localStorage.getItem('score')){
+	$('#bestscore').text('Score : ' + localStorage.getItem('score'));
+}
+else{
+	localStorage.setItem('score', 0);
+	$('#bestscore').text('Score : 0');
+}
+
+function decompte(){
 	//sert uniquement à retirer le S à seconde quand on affiche
 	if(compte <= 1) {
 		pluriel = "";
@@ -8,100 +17,72 @@ function decompte()
 		pluriel = "s";
 	}
 	// sert à modifier l'affichage du mot "secondes"
-	document.getElementById("compt").innerHTML = compte + " seconde" + pluriel;
+	$("#compt")[0].innerHTML = compte + " seconde" + pluriel;
 
 	// si le compteur est = 0 alors on affiche alerte "c'est fini"
 	if(compte == 0 || compte < 0) {
 		compte = 0;
 		//ClearInterval interrompt la boucle à la fin du compteur
 		clearInterval(timer);
-		alert("C'est terminé Votre Score est de : " +compteurDePoint);
+		clearInterval(t);
+		
+		if(compteurDePoint > localStorage.getItem('score')){
+			localStorage.setItem('score', compteurDePoint);
+		$('#bestscore').text('Score : ' + compteurDePoint)
+		}
+		
+		
 	}
 	//on diminue de 1 seconde
 	compte--;
 }
-var timer = setInterval('decompte()',1000); 
+
+let timer = setInterval(decompte,1000);
+let t = setInterval(positionAleatoire,1000);
+
+// MARTEAU SOURIS
 
 
-$('div').mouseup(function() {
 
-	$('div').css({
+/* $('div').mouseup(function() {
+	$('.canvas').css({
 		cursor: "url('../images/Marteau_TFH.png'), pointer",
 	});
 });
 
-$('div').mousedown(function() {
 
-	$('div').css({
+$('div').mousedown(function() {
+	$('.canvas').css({
 		cursor: "url('../images/Marteau.Click.png'), pointer",
 	});
-}); 
-
-
-function positionAleatoire() {
-	var maxWidth = $('div').width();
-	var maxHeight = $('div').height();
-	var position = {
-		x: Math.floor(Math.random() * maxWidth), // Donne à la clé X de l'objet une valeur aléatoire entre 0 et 1440;
-		y: Math.floor(Math.random() * maxHeight) // Donne à la clé Y de l'objet une valeur aléatoire entre 0 et 1440;
-	}
-
-	$('img').css({
-		left: position.x,
-		top: position.y
-	});
-}
-setInterval('positionAleatoire()',1500); 
-
-var compteurDePoint=0;
-$('img').click(function(){
-	compteurDePoint ++;
-	$('#score')[0].innerHTML = "Score : " + compteurDePoint;
-
-
 });
+*/
 
+// POSITION ALEATOIRE TAUPE
+function positionAleatoire() {
+	let maxWidth = $('.canvas').width();
+	let maxHeight = $('.canvas').height();
+	var x = 0;
+	x = Math.floor(Math.random() * 9);
 
+	var cases = document.querySelectorAll('.case');
+	for(var i = 0; i < cases.length ; i++){
+		$('.case' + [i]).text('');
+	}
+	
+	$('.case' + [x]).html('<div id="taupe"><img src="images/Taupiqueur.png" alt="topiqueur"/></div>');
+	console.log(x);
+$('.case' + [x]).click(function(){
 
+JouerSon();
+	compteurDePoint++;
+	$('#score').text(compteurDePoint);
+});
+	
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function JouerSon() {
+            var sound = document.getElementById("beep");
+            sound.play();
+        }
 
